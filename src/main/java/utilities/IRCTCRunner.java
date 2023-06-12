087ede988084b03fe48f1d2804c7c99770904c08
Siva Kumar <SivaKumar.g1991@gmail.com>
2023-06-12 21:16:33 +0530

    pom xml update

--- src/main/java/utilities/IRCTCRunner.java
+++ src/main/java/utilities/IRCTCRunner.java
@@ -4,9 +4,10 @@ import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="./src/test/java/features/IRCTC.feature", glue="stepDefinations")
-public class IRCTCRunner {
+public class IRCTCRunner{
 
 }
